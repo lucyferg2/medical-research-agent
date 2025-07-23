3df1279dca174ce90c901d21ae6c2093192a650f
lucyferg2 <lucyferguson02@gmail.com>
2025-07-23 11:23:14 +0100

    Delete app directory

--- app/agents/__init__.py
+++
@@ -1,4 +0,0 @@
-from .medical_agents import AgentOrchestrator, MedicalResearchAgent
-from .research_tools import MedicalResearchTools
-
-__all__ = ['AgentOrchestrator', 'MedicalResearchAgent', 'MedicalResearchTools']
